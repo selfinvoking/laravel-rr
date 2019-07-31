82db8338e4bece32909c5eea3613250f4bace905
Carl Walsh <carl@selfinvoking.com>
2019-07-31 22:44:02 +0100

    Send dumper output to STDERR

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Pass laravel dump output to STDERR for roadrunner
+        if (App::environment('local')) {
+            VarDumper::setHandler(function ($var) {
+                $var = (new VarCloner())->cloneVar($var);
+                return (new CliDumper(STDERR))->dump($var);
+            });
+        }
     }
 }
